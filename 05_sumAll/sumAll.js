f5cc699d57afb78c02c9ef062d85b7b47582b6eb
Kwadwo Firempong-Boakye <firempong-boakye@outlook.com>
2022-07-29 15:47:27 +0000

    Complete all tests for T.O.P Assignment 2 - No.5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,34 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+    let sumNum = 0;
 
+    let biggerNum;
+    let smallerNum;
+
+    if (typeof a != "number" || typeof b != "number"){
+        return "ERROR";
+    } else if (a < 0 || b < 0) {
+        return "ERROR";
+    } else if (a > b) {
+        biggerNum = a;
+        smallerNum = b;
+    } else if (b > a) {
+        biggerNum = b;
+        smallerNum = a;
+    } else {
+        return a;
+    }
+
+    
+
+    for (let i = biggerNum; i >= smallerNum; i--) {
+        sumNum += i;
+    }
+
+    return sumNum;
 };
+
+sumAll(6, 15);
+
 
 // Do not edit below this line
 module.exports = sumAll;
