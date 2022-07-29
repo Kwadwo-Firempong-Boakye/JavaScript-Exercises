624668e70a7ca6f4cedca206f4448d2af6224123
Kwadwo Firempong-Boakye <firempong-boakye@outlook.com>
2022-07-29 16:29:46 +0000

    Complete T.O.P assignment 2 - No.6 - passed tests

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,24 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    // check if year % 4 = 0 ? progress ELSE nope!
+    // check if year % 100 = 0 ? still progress
+    // check if year % 400 = 0 ? Yes! ELSE nope!
 
+    if (year % 4 == 0) {
+        if (year % 100 == 0) {
+            if (year % 400 ==0) {
+                return true;
+            } else {
+                return false;
+            }
+        } else {
+            return true;
+        }
+    } else {
+        return false;
+    }
 };
+
+leapYears(2000);
 
 // Do not edit below this line
 module.exports = leapYears;
