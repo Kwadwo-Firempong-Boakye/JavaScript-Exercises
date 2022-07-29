0c9f5890d5d1dee842fa80a795ae9779013bd63d
Kwadwo Firempong-Boakye <firempong-boakye@outlook.com>
2022-07-29 16:54:56 +0000

    Complete Assignment 2 - no.7 - all tests passed

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,10 +1,15 @@
-const ftoc = function() {
-
+const ftoc = function(f) {
+let celcius = ((5/9) * (f -32));
+return +(celcius.toFixed(1));
 };
 
-const ctof = function() {
-
+const ctof = function(c) {
+let fahrenheit = (c * (9/5) + 32);
+return +(fahrenheit.toFixed(1));
 };
+
+"fah-to-cel", ftoc(100);
+"cel-to-fah", ctof(0);
 
 // Do not edit below this line
 module.exports = {
