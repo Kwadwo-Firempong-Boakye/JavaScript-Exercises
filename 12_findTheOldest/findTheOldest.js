e9634c1b58484ab4b04e217f83baf848faf8a3dc
Kwadwo Firempong-Boakye <firempong-boakye@outlook.com>
2022-08-17 21:32:17 +0000

    refactor using reduce for cleaner code

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,4 +1,42 @@
+/*const people = [
+	{
+		name: "Carly",
+		yearOfBirth: 1942,
+		yearOfDeath: 1970,
+	},
+	{
+		name: "Ray",
+		yearOfBirth: 1962,
+		yearOfDeath: 2011,
+	},
+    {
+		name: "Test",
+		yearOfBirth: 1910,
+		yearOfDeath: 1941,
+	},
+	{
+		name: "Jane",
+		yearOfBirth: 1912,
+		yearOfDeath: 1941,
+	},
+];*/
+
+const getAge = function (death, birth) {
+    if(!death) {
+        death = new Date().getFullYear();
+    }
+    return death - birth;
+};
+
 const findTheOldest = function (arr) {
+	return arr.reduce((oldest, current) => {
+		const oldestAge = getAge(oldest.yearOfDeath, oldest.yearOfBirth);
+		const currentAge = getAge(current.yearOfDeath, current.yearOfBirth);
+        return currentAge > oldestAge ? current : oldest;
+	});
+
+	
+	/*
 	let presentYear = new Date().getFullYear();
 
 	let ages = arr.map((item) => {
@@ -21,8 +59,9 @@ const findTheOldest = function (arr) {
 	}
 	// console.log(oldestIndex);
 	return arr[oldestIndex];
+    */
 };
 
-// console.log(findTheOldest(people));
+console.log(findTheOldest(people));
 // Do not edit below this line
 module.exports = findTheOldest;
